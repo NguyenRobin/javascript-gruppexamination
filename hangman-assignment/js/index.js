83c7ca6c9954a43d972ddd5e70b386b792f98cfd
NguyenRobin <robin.nnguyen@hotmail.com>
2022-11-03 14:27:19 +0100

    refactor

--- hangman-assignment/js/index.js
+++ hangman-assignment/js/index.js
@@ -1,9 +1,160 @@
-/**
- För att toggla SVG:en
- document.querySelector('figure').classList.add('scaffold')
- document.querySelector('figure').classList.add('head')
- document.querySelector('figure').classList.add('body')
- document.querySelector('figure').classList.add('arms')
- document.querySelector('figure').classList.add('legs')
+// "use strict";
 
- */
+const secretWordElement = document.querySelector(".hangman-text");
+const popUpWindowElement = document.querySelector(".pop-up-window");
+const buttonPlayAgain = document.querySelector("button");
+const wrongLetterElement = document.querySelector(".wrong__letters-container");
+const popUpWindowTextElement = document.querySelector(".pop-up-heading");
+const timerElement = document.querySelector(".timer-seconds");
+
+//  För att toggla SVG:en
+document.querySelector("figure").classList.add("scaffold");
+// document.querySelector("figure").classList.add("head");
+// document.querySelector("figure").classList.add("body");
+// document.querySelector("figure").classList.add("arms");
+// document.querySelector("figure").classList.add("legs");
+
+// List of all words that can randomly be shown.
+const wordList = ["pumpkin", "ghost", "witch", "skeleton", "dracula"];
+
+// Random word from our wordList array
+let secretWord = wordList[Math.trunc(Math.random() * wordList.length)];
+console.log(secretWord);
+
+const parts = ["head", "body", "arms", "legs"];
+let currentParts = parts.slice(); // makes a copy of the parts array
+
+const correctWord = []; // stores the correct letters user guesses
+const incorrectLetters = []; // Stores the wrong guess
+
+let countDownTimer; // Needs to be global so we can access it it each function.
+
+/******************************************************************************/
+/* PRINTS THE CORRECT LETTER */
+/******************************************************************************/
+function printSecretWord() {
+  const eachLetterOfSecretWord = secretWord.split("");
+  secretWordElement.innerHTML = "";
+  for (let i = 0; i < eachLetterOfSecretWord.length; i++) {
+    if (correctWord.includes(eachLetterOfSecretWord[i])) {
+      secretWordElement.innerHTML += `<span class="hangman-letter">${eachLetterOfSecretWord[i]}</span>`;
+    } else {
+      secretWordElement.innerHTML += `<span class="hangman-letter">${""}</span>`;
+    }
+  }
+  // secretWordElement.innerHTML = `${secretWord
+  //   .split("")
+  //   .map(
+  //     (eachLetter) =>
+  //       `<span class="hangman-letter">${
+  //         correctWord.includes(eachLetter) ? eachLetter : ""
+  //       }</span>`
+  //   )
+  //   .join("")}`;
+  console.log(secretWordElement.textContent);
+  console.log(secretWord);
+
+  // if guess i correct, user wins
+  if (secretWordElement.innerText === secretWord) {
+    console.log(secretWordElement.innerText);
+    if (countDownTimer) clearInterval(countDownTimer);
+    popUpWindowElement.classList.remove("hidden");
+  }
+}
+printSecretWord();
+
+/******************************************************************************/
+/* UPDATES EACH WRONG LETTER */
+/******************************************************************************/
+function printWrongLettersElement() {
+  //Display wrong letters
+  wrongLetterElement.innerHTML = `
+  ${incorrectLetters.length > 0 ? "<p>Wrong picks</p>" : ""}
+  ${incorrectLetters.map((eachLetter) => `<span"> ${eachLetter}</span>`)}
+  `;
+
+  wrongLetterElement.classList.remove("hidden");
+
+  // Display parts each time guess is wrong
+  let partToShow = currentParts.shift();
+  document.querySelector("figure").classList.add(partToShow);
+
+  // Player losses if all the part been displayed.
+  if (currentParts.length === 0) {
+    if (countDownTimer) clearInterval(countDownTimer);
+    popUpWindowElement.classList.remove("hidden");
+    popUpWindowTextElement.textContent = "You lost! 🎃";
+  }
+}
+
+/******************************************************************************/
+/* REGISTER KEY PRESSED */
+/******************************************************************************/
+function guess() {
+  document.addEventListener("keydown", function (event) {
+    console.log(event.key);
+    if (secretWord.includes(event.key)) {
+      if (!correctWord.includes(event.key)) {
+        correctWord.push(event.key);
+        printSecretWord();
+      }
+    } else if (!incorrectLetters.includes(event.key)) {
+      incorrectLetters.push(event.key);
+      printWrongLettersElement();
+    }
+  });
+  countDownTimer = startTimer();
+}
+guess();
+
+/******************************************************************************/
+/* COUNTDOWN TIMER */
+/******************************************************************************/
+function startTimer() {
+  const tickTack = function () {
+    const min = String(Math.trunc(time / 60)).padStart(2, 0); // Starting min
+    const sec = String(time % 60).padStart(2, 0); // Re
+
+    timerElement.textContent = `${min}:${sec}`;
+
+    // Display popup window if counter is at 0
+    if (time === 0) {
+      clearInterval(countDownTimer);
+      popUpWindowElement.classList.remove("hidden");
+      popUpWindowTextElement.textContent = "You lost! 🎃";
+    }
+
+    // Decrease our timer every sec
+    time--;
+    // console.log(time--);
+  };
+
+  let time = 10;
+
+  tickTack();
+
+  // call setInterval function each sec
+  const countDownTimer = setInterval(tickTack, 1000);
+  return countDownTimer;
+}
+
+/******************************************************************************/
+/* PLAY AGAIN BUTTON */
+/******************************************************************************/
+buttonPlayAgain.addEventListener("click", function () {
+  correctWord.splice(0); // Sets the array back to be empty again.
+  incorrectLetters.splice(0);
+  secretWord = wordList[Math.trunc(Math.random() * wordList.length)];
+  console.log(secretWord);
+  popUpWindowElement.classList.add("hidden");
+  wrongLetterElement.classList.add("hidden");
+  popUpWindowTextElement.textContent = "You won! 👻";
+
+  currentParts = parts.slice();
+  document
+    .querySelector("figure")
+    .classList.remove("head", "body", "arms", "legs");
+  printSecretWord();
+  if (countDownTimer) clearInterval(countDownTimer);
+  countDownTimer = startTimer();
+});
